789653e6203482541d317b4a778fda6fd2ac16b0
Lubava <Lwm28@rambler.ru>
2022-10-22 10:20:10 +0300

    сделано домашнее задание2

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main{
+
+
+    private static boolean cat;
+
+    public static void main (String[] args) {
+    double dog = 8;
+    System.out.println (dog) ;
+
+
+
+
+double cat = 3.6;
+System.out.println (cat);
+    var paper=763789;
+    System.out.println (paper);
+    dog=dog+4;
+   System.out.println(dog);
+    cat=cat+4;
+    System.out.println(cat);
+   paper=paper+4;
+    System.out.println(paper);
+    dog=dog-3.5;
+    System.out.println(dog);
+    cat=cat-3.6;
+    System.out.println(cat);
+   paper=paper-7639;
+    System.out.println(paper);
+    var friend=19;
+    System.out.println(friend);
+    friend=friend+2;
+    System.out.println(friend);
+    friend=friend/7;
+    System.out.println(friend);
+    double frog=3.5;
+    System.out.println(frog);
+    frog=frog*10;
+    System.out.println(frog);
+    frog=frog/3.5;
+    System.out.println(frog);
+    frog=frog+4;
+    System.out.println(frog);
+
+   double weightFirst=78.8;
+    double weightSecond=80.2;
+        double weight = weightSecond + weightFirst;
+    System.out.println("общий вес бойцов " + weight + "кг ");
+        double differenceWeight=(weightSecond-weightFirst);
+        System.out.println("разница в весе " +differenceWeight + "кг ");
+     double differenceWeight2 = (weightSecond-weightFirst) % weight;
+    System.out.println("разница в весе" + differenceWeight2 + "кг ");
+
+    int workingHours=640;
+    System.out.println(workingHours);
+    int workingDay=8;
+    System.out.println(workingDay);
+    int fellouWoker=(workingHours/workingDay);
+    System.out.println(fellouWoker);
+    int fellouWokerNew=94;
+    System.out.println(fellouWokerNew);
+    int newHours=(fellouWokerNew*workingDay);
+    System.out.println(newHours);
+    int allHours=(workingHours+newHours);
+    System.out.println(allHours);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
